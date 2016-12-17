7646e992dbdfa1bed56b80f6cc21b9f371f1c6c8
Sergey Vishnevsky <svishnevskij@gmail.com>
2016-12-17 12:10:12 +0300

    Added migration

---
+++ Sources/OnlineShowcase.Data.EF/Migrations/20161217090910_AddedImages.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace OnlineShowcase.Data.EF.Migrations
+{
+    public partial class AddedImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(nullable: false, defaultValueSql: "getutcdate()"),
+                    Name = table.Column<string>(nullable: false),
+                    Path = table.Column<string>(nullable: false),
+                    Reference = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("ImageId", x => x.ImageId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_Path",
+                table: "Images",
+                column: "Path");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Products");
+        }
+    }
+}
